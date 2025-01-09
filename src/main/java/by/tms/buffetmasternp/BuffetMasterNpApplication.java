7d0a45341f9a844c94df0dcb92b6222fd4601683
Raman <rdubouski@gmail.com>
2025-01-09 12:02:21 +0300

    Initial commit

---
+++ src/main/java/by/tms/buffetmasternp/BuffetMasterNpApplication.java
@@ -0,0 +1,13 @@
+package by.tms.buffetmasternp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuffetMasterNpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuffetMasterNpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/tms/buffetmasternp/BuffetMasterNpApplicationTests.java
@@ -0,0 +1,13 @@
+package by.tms.buffetmasternp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuffetMasterNpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
